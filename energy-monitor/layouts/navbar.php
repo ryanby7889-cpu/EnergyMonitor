871f03845477c29f122203966161d8ce6534d2e1
ryanby7889-cpu <ryanby7889@gmail.com>
2026-09-05 18:45:40 +0700

    chore: add dashboard navbar

---
+++ energy-monitor/layouts/navbar.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-success shadow">
+<div class="container-fluid">
+<a class="navbar-brand fw-bold">☪ MARY EMS</a>
+<div class="ms-auto">
+<span class="text-white">🟢 ONLINE</span>
+<li class="nav-item d-inline-block ms-3">
+<a class="nav-link d-inline" href="../alarm/alarm_history.php">
+<span id="alarmBadge" class="badge bg-success">🔔 0 Alarm</span>
+</a>
+</li>
+</div>
+</div>
+</nav>
