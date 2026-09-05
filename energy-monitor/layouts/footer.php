aa94d79b5cfc76259ec097d52fb0ea8ebdc57bef
ryanby7889-cpu <ryanby7889@gmail.com>
2026-09-05 18:45:44 +0700

    chore: add shared page footer

---
+++ energy-monitor/layouts/footer.php
@@ -0,0 +1,6 @@
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script src="/energy-monitor/assets/js/dashboard.js"></script>
+<script src="../assets/js/alarm.js"></script>
+</body>
+</html>
